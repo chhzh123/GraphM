632ecdf2a5180a2c0dc6888a06868573d12102a2
chhzh123 <982270930@qq.com>
2020-04-09 18:42:50 +0800

    Fix bugs in M-BFS

--- GridGraph-M/examples/M-BFS.cpp
+++ GridGraph-M/examples/M-BFS.cpp
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "core/graph.hpp"
-
+#include <vector>
 
 #define MAX_DEPTH 100000000
 
@@ -29,10 +29,11 @@ int main(int argc, char ** argv)
     double begin_time = get_time();
     Graph graph(path);
     graph.set_memory_bytes(memory_bytes);
+    printf("Vertices: %d Edges: %ld\n",graph.vertices,graph.edges);
 
     long vertex_data_bytes = (long)graph.vertices * (sizeof(VertexId) + sizeof(float) + sizeof(float));
     graph.set_vertex_data_bytes(vertex_data_bytes);
-    VertexId active_vertices = 8;
+    VertexId active_vertices = PRO_NUM;
 
     //bfs
     Bitmap * active_in_bfs[PRO_NUM];
@@ -46,11 +47,13 @@ int main(int argc, char ** argv)
         parent[i].init(graph.path+"/parent"+std::to_string(i), graph.vertices);
         //graph.set_vertex_data_bytes( graph.vertices * sizeof(VertexId) );
         int start_bfs = 91 * (i+1);
+        if (start_bfs >= graph.vertices)
+            start_bfs = i + 1;
         active_out_bfs[i]->clear();
         active_out_bfs[i]->set_bit(start_bfs);
         parent[i].fill(-1);
         parent[i][start_bfs] = start_bfs;
-        active_out_bfs[i]->fill();
+        // active_out_bfs[i]->fill();
     }
     #pragma omp barrier
 
@@ -61,18 +64,25 @@ int main(int argc, char ** argv)
     for (int iter = 0; active_vertices != 0; iter++)
     {
         printf("%7d: %d\n", iter, active_vertices);
-        graph.hint(parent[0]);
+        for (int i = 0; i < PRO_NUM; i++)
+            graph.hint(parent[i]);
 
         if(active_vertices!=0){
-            graph.clear_should_access_shard(graph.should_access_shard_sssp);
             graph.clear_should_access_shard(graph.should_access_shard_bfs);
-            graph.clear_should_access_shard(graph.should_access_shard_wcc);
 
-            std::swap(active_in_bfs[0], active_out_bfs[0]);
-            active_out_bfs[0]->clear();
-            graph.get_should_access_shard(graph.should_access_shard_bfs, active_in_bfs[0]);
+            for (int i = 0; i < PRO_NUM; ++i){
+                std::swap(active_in_bfs[i], active_out_bfs[i]);
+                active_out_bfs[i]->clear();
+                graph.get_should_access_shard(graph.should_access_shard_bfs, active_in_bfs[i]);
+            }
         }
 
+#ifdef DEBUG
+        for (int i = 0; i < PRO_NUM; ++i)
+            active_in_bfs[i]->print(10);
+#endif
+
+        std::vector<Edge> edges;
         graph.get_global_should_access_shard(graph.should_access_shard_wcc, graph.should_access_shard_pagerank,
                                              graph.should_access_shard_bfs,graph.should_access_shard_sssp);
         active_vertices = graph.stream_edges<VertexId>([&](Edge & e){
@@ -84,12 +94,17 @@ int main(int argc, char ** argv)
         },[&](Edge & e){
             //bfs
             int return_state = 0;
-            if(active_in_bfs[0] ->get_bit(e.source)){
-                for(int i = 0; i < PRO_NUM; i++){
+            for(int i = 0; i < PRO_NUM; i++){
+                if(active_in_bfs[i] ->get_bit(e.source)){
                     if (parent[i][e.target]==-1){
                         if (cas(&parent[i][e.target], -1, e.source)){
-                            active_out_bfs[0]->set_bit(e.target);
-                            return_state = 1;
+                            active_out_bfs[i]->set_bit(e.target);
+                            bool flag = false;
+                            for (int j = 0; j < PRO_NUM; j++)
+                                if (i != j && active_out_bfs[j]->get_bit(e.target))
+                                    flag = true;
+                            if (!flag)
+                                return_state = 1;
                         }
                     }
                 }
